1c50fae6cd0ca84e4647a586db462553d312279a
Brad Richardson <bradrichardsonmail@gmail.com>
2020-06-11 23:11:32 -0600

    Move map to own fragment

--- app/src/main/java/com/carswaddle/carswaddleandroid/ui/activities/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/carswaddle/carswaddleandroid/ui/activities/dashboard/DashboardFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.carswaddle.carswaddleandroid.R
-import com.carswaddle.carswaddleandroid.ui.activities.MapsActivity
+import com.carswaddle.carswaddleandroid.ui.activities.schedule.MapsActivity
 
 class DashboardFragment : Fragment() {
 
