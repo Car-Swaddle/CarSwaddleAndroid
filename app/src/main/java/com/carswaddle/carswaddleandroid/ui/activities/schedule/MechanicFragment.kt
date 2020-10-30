059674e720319da0b9d8c9739a4281a1c687d164
Brad Richardson <bradrichardsonmail@gmail.com>
2020-10-29 23:53:40 -0600

    Cleaner snap scroll, remove old code

--- app/src/main/java/com/carswaddle/carswaddleandroid/ui/activities/schedule/MechanicFragment.kt
+++ app/src/main/java/com/carswaddle/carswaddleandroid/ui/activities/schedule/MechanicFragment.kt
@@ -1,22 +1,18 @@
 package com.carswaddle.carswaddleandroid.ui.activities.schedule
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import com.carswaddle.carswaddleandroid.Extensions.addDays
 import com.carswaddle.carswaddleandroid.Extensions.toJavaCalendar
 import com.carswaddle.carswaddleandroid.Extensions.today
@@ -26,18 +22,14 @@ import com.carswaddle.carswaddleandroid.services.serviceModels.Point
 import com.haibin.calendarview.Calendar
 import com.haibin.calendarview.CalendarView
 import java.text.DateFormatSymbols
+import java.util.*
 import java.util.Calendar.*
-import java.util.Locale
-import kotlin.math.max
 import java.util.Calendar as KotlinCalendar
 
 
 class MechanicFragment(val point: Point) : Fragment() {
 
-    private var itemWidth: Int = 0
-    private var times: List<String> = listOf("10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM")
     private var spanCount = 4
-    private var rawSpanCount = 12
 
     private lateinit var monthYearTextView: TextView
 
@@ -63,26 +55,28 @@ class MechanicFragment(val point: Point) : Fragment() {
         mechanicViewAdapter = MyMechanicRecyclerViewAdapter()
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.mechanic_list_container)
-        with (recyclerView) {
+        with(recyclerView) {
             this.adapter = mechanicViewAdapter
             this.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-            PagerSnapHelper().attachToRecyclerView(this)
+            val snapHelper = PagerSnapHelper()
+            snapHelper.attachToRecyclerView(recyclerView)
+            this.onFlingListener = snapHelper
         }
-        runJustBeforeBeingDrawn(recyclerView, Runnable {
-            if (itemWidth > 0 || recyclerView.layoutManager!!.itemCount < 1) {
-                return@Runnable
-            }
-            itemWidth = recyclerView.layoutManager!!.findViewByPosition(0)!!.width
-            recyclerView.layoutManager = MechanicLinearLayoutManager(context, requireActivity().window.decorView.width, itemWidth)
-        })
 
         monthYearTextView = view.findViewById(R.id.month_year_text_view)
-        updateMonthYear(java.util.Calendar.getInstance().get(java.util.Calendar.MONTH), java.util.Calendar.getInstance().get(java.util.Calendar.YEAR))
+        updateMonthYear(getInstance().get(MONTH), getInstance().get(YEAR))
         calendarView = view.findViewById(R.id.calendar_view)
 
         val minCalendar = Calendar().today().addDays(1)
         val maxCalendar = Calendar().today().addDays(7)
-        calendarView.setRange(minCalendar.year, minCalendar.month, minCalendar.day, maxCalendar.year, maxCalendar.month, maxCalendar.day)
+        calendarView.setRange(
+            minCalendar.year,
+            minCalendar.month,
+            minCalendar.day,
+            maxCalendar.year,
+            maxCalendar.month,
+            maxCalendar.day
+        )
         calendarView.scrollToCalendar(minCalendar.year, minCalendar.month, minCalendar.day)
 
         calendarView.setOnCalendarSelectListener(object : CalendarView.OnCalendarSelectListener {
@@ -103,25 +97,28 @@ class MechanicFragment(val point: Point) : Fragment() {
         with(timeRecycler) {
             this.adapter = timeSlotViewAdapter
             val gridLayoutManager = GridLayoutManager(context, spanCount)
-//            gridLayoutManager.spanSizeLookup = MySizeLookup(rawSpanCount, spanCount, times.size)
             this.layoutManager = gridLayoutManager
-//            this.addItemDecoration(EqualSpacingItemDecoration(16))
         }
 
-        mechanicViewModel.mechanics.observe(viewLifecycleOwner, Observer<List<MechanicListElements>> { mechanicElements ->
-            this.mechanicViewAdapter.mechanicElements = mechanicElements
-            val firstMechanicId = mechanicElements.first()?.mechanic.id
-            if (firstMechanicId != null) {
-                mechanicViewModel.loadTimeSlots(firstMechanicId) {
-                    activity?.runOnUiThread {
-                        var tomorrow = KotlinCalendar.getInstance()
-                        tomorrow.add(KotlinCalendar.DAY_OF_YEAR, 1)
-                        updateTimeSlots(tomorrow)
+        mechanicViewModel.mechanics.observe(
+            viewLifecycleOwner,
+            Observer<List<MechanicListElements>> { mechanicElements ->
+                this.mechanicViewAdapter.mechanicElements =
+                    mechanicElements
+                val firstMechanicId = mechanicElements.first()?.mechanic.id
+                if (firstMechanicId != null) {
+                    mechanicViewModel.loadTimeSlots(firstMechanicId) {
+                        activity?.runOnUiThread {
+                            // TODO - not working
+                            recyclerView.scrollToPosition(1) // 0 is padding, 1 is first item
+                            var tomorrow = KotlinCalendar.getInstance()
+                            tomorrow.add(KotlinCalendar.DAY_OF_YEAR, 1)
+                            updateTimeSlots(tomorrow)
+                        }
+
                     }
-                    
                 }
-            }
-        })
+            })
         
         return view
     }
@@ -142,103 +139,6 @@ class MechanicFragment(val point: Point) : Fragment() {
     private fun updateMonthYear(month: Int, year: Int) {
         val monthStr = DateFormatSymbols(Locale.getDefault()).months[month];
         monthYearTextView.text = monthStr + " " + year
-    }
-
-    class MySizeLookup(val rawSpanCount: Int, val spanCount: Int, val itemCount: Int) : SpanSizeLookup() {
-        override fun getSpanSize(position: Int): Int {
-            if (itemCount == 0 || spanCount == 0 || rawSpanCount == 0) {
-                // Invalid, default span
-                return rawSpanCount / spanCount
-            }
-            val excessItems = itemCount % spanCount
-            val lastRowIndexStart = if (excessItems == 0) itemCount - spanCount else itemCount - excessItems
-            val itemsInLastRow = itemCount - lastRowIndexStart
-            if (position < lastRowIndexStart) {
-                // Take up full row (3 span sizes each so 4 across)
-                return rawSpanCount / spanCount
-            } else {
-                return rawSpanCount / spanCount
-//                return rawSpanCount / itemsInLastRow
-            }
-        }
-    }
-
-    class EqualSpacingItemDecoration constructor(
-        private val spacing: Int,
-        private var displayMode: Int = -1
-    ) :
-        ItemDecoration() {
-
-        override fun getItemOffsets(
-            outRect: Rect,
-            view: View,
-            parent: RecyclerView,
-            state: RecyclerView.State
-        ) {
-            val position = parent.getChildViewHolder(view).adapterPosition
-            val itemCount = state.itemCount
-            val layoutManager = parent.layoutManager
-            setSpacingForDirection(outRect, layoutManager, position, itemCount)
-        }
-
-        private fun setSpacingForDirection(
-            outRect: Rect,
-            layoutManager: RecyclerView.LayoutManager?,
-            position: Int,
-            itemCount: Int
-        ) {
-
-            // Resolve display mode automatically
-            if (displayMode == -1) {
-                displayMode = resolveDisplayMode(layoutManager)
-            }
-            when (displayMode) {
-                HORIZONTAL -> {
-                    outRect.left = spacing
-                    outRect.right = if (position == itemCount - 1) spacing else 0
-                    outRect.top = spacing
-                    outRect.bottom = spacing
-                }
-                VERTICAL -> {
-                    outRect.left = spacing
-                    outRect.right = spacing
-                    outRect.top = spacing
-                    outRect.bottom = if (position == itemCount - 1) spacing else 0
-                }
-                GRID -> if (layoutManager is GridLayoutManager) {
-                    val cols = layoutManager.spanCount
-                    val rows = itemCount / cols
-                    outRect.left = spacing
-                    outRect.right = if (position % cols == cols - 1) spacing else 0
-                    outRect.top = spacing
-                    outRect.bottom = if (position / cols == rows - 1) spacing else 0
-                }
-            }
-        }
-
-        private fun resolveDisplayMode(layoutManager: RecyclerView.LayoutManager?): Int {
-            if (layoutManager is GridLayoutManager) return GRID
-            return if (layoutManager!!.canScrollHorizontally()) HORIZONTAL else VERTICAL
-        }
-
-        companion object {
-            const val HORIZONTAL = 0
-            const val VERTICAL = 1
-            const val GRID = 2
-        }
-
-    }
-
-    private fun runJustBeforeBeingDrawn(view: View, runnable: Runnable) {
-        val preDrawListener: ViewTreeObserver.OnPreDrawListener =
-            object : ViewTreeObserver.OnPreDrawListener {
-                override fun onPreDraw(): Boolean {
-                    view.viewTreeObserver.removeOnPreDrawListener(this)
-                    runnable.run()
-                    return true
-                }
-            }
-        view.viewTreeObserver.addOnPreDrawListener(preDrawListener)
     }
 
 }
--- app/src/main/java/com/carswaddle/carswaddleandroid/ui/activities/schedule/MyMechanicRecyclerViewAdapter.kt
+++ app/src/main/java/com/carswaddle/carswaddleandroid/ui/activities/schedule/MyMechanicRecyclerViewAdapter.kt
@@ -1,14 +1,14 @@
 package com.carswaddle.carswaddleandroid.ui.activities.schedule
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatRatingBar
+import androidx.recyclerview.widget.RecyclerView
 import com.carswaddle.carswaddleandroid.R
-import com.carswaddle.carswaddleandroid.data.mechanic.Mechanic
 import com.carswaddle.carswaddleandroid.data.mechanic.MechanicListElements
+
 
 /**
  * [RecyclerView.Adapter] that can display a [DummyItem].
@@ -28,22 +28,47 @@ class MyMechanicRecyclerViewAdapter(
     }
     
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+//        return if (viewType == VIEW_TYPE_ITEM) {
+//            val v: View = LayoutInflater.from(parent.context)
+//                .inflate(R.layout.list_item, parent, false)
+//            ViewHolder(v)
+//        } else {
+//            val v: View = LayoutInflater.from(parent.context)
+//                .inflate(R.layout.list_item_padding, parent, false)
+//            ViewHolder(v)
+//        }
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.fragment_mechanic_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = mechanicElements[position]
+        if (getItemViewType(position) != VIEW_TYPE_ITEM) {
+            // TOOD - reset everything or set alpha to 0
+            holder.itemView.alpha = 0f
+            return
+        }
+        holder.itemView.alpha = 1f
+        val item = mechanicElements[position - 1] // Offset for initial padding
         // TODO - set image
         holder.nameTextView.text = item.user.displayName()
         holder.ratingBar.rating = item.mechanic.averageRating?.toFloat() ?: 0.0F
-        val roundedValue = Math.round(item.mechanic.averageRating?.toFloat() ?: 0.0F).times(10).div(10).toFloat()
+        val roundedValue = Math.round(item.mechanic.averageRating?.toFloat() ?: 0.0F).times(10).div(
+            10
+        ).toFloat()
         holder.ratingTextView.text = "$roundedValue avg from ${item.mechanic.numberOfRatings} ratings"
         holder.servicesCompletedTextView.text = "${item.mechanic.autoServicesProvided} services completed"
     }
 
-    override fun getItemCount(): Int = mechanicElements.size
+    override fun getItemCount(): Int = mechanicElements.size + 2 // Padding at start and end
+
+    //http://www.plattysoft.com/2015/06/16/snapping-items-on-a-horizontal-list/
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0 || position == itemCount - 1) {
+            VIEW_TYPE_PADDING
+        } else VIEW_TYPE_ITEM
+    }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val nameTextView: TextView = view.findViewById(R.id.name)
@@ -55,5 +80,11 @@ class MyMechanicRecyclerViewAdapter(
 //            return super.toString() + " '" + contentView.text + "'"
 //        }
     }
-    
+
+    companion object {
+        private const val VIEW_TYPE_PADDING = 1
+        private const val VIEW_TYPE_ITEM = 2
+    }
+
 }
+
